bca9e59d0cb8836c671b3578a2b5d2235ef92619
mungvv <mung.vu@bnksolution.com>
2024-03-21 20:08:07 +0700

    update

--- main/main.js
+++ main/main.js
@@ -35,9 +35,8 @@ const dbRef = ref(db, "users/");
 function logData() {
   console.log(123);
 }
-$(document).ready(function() {
-  logData()
-})
+
+
 
 function getUser() {
   let listUser = "";
@@ -53,22 +52,32 @@ function getUser() {
                       <td>${childData.last_name}</td>
                       <td>  
                       <button id="add"  onclick="console.log(1233)">add</button>
-                      <button id="update"  onclick="updateUserData(${childKey})">update</button>
+                      <button id="update"  onclick="selectData(${childKey})">update</button>
                       <button id="remove"  onclick="removeUserData(${childKey})">delete</button>
                       </td>
                     </tr>`;
       });
       document.getElementById("tablebody").innerHTML = listUser;
     });
+    
   } catch (error) {
     console.log(error);
   }
 }
 getUser();
 
-
+function selectData(id) {
+  const dataref = ref(db);
+  get(child(dataref, "users/" + id)).then((snapshot) => {
+    if (snapshot.exists()) {
+      firstName.value = snapshot.val().first_name;
+      lastName.value = snapshot.val().last_name;
+    } else {
+      alert("no data");
+    }
+  });
+}
 const updateUserData = (id) => {
-  console.log("id", id);
   const firstName = document.getElementById("firstName").value;
   const lastName = document.getElementById("lastName").value;
   update(ref(db, "users/" + id), {
@@ -85,17 +94,7 @@ function writeUserData() {
   }).catch((err) => console.log(err));
 }
 
-function selectData() {
-  const dataref = ref(db);
-  get(child(dataref, "users/" + idUser.value)).then((snapshot) => {
-    if (snapshot.exists()) {
-      firstName.value = snapshot.val().first_name;
-      lastName.value = snapshot.val().last_name;
-    } else {
-      alert("no data");
-    }
-  });
-}
+
 
 function pushGenerated() {
   set(push(ref(db, "users/")), {
@@ -109,7 +108,7 @@ function removeUserData(id) {
 }
 
 insbtn.addEventListener("click", writeUserData);
-secbtn.addEventListener("click", selectData);
+secbtn.addEventListener("click", updateUserData);
 pushbtn.addEventListener("click", pushGenerated);
 
 
