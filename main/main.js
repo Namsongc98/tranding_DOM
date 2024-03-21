a3ebb1cf58598095bd142fb16509db4fadeb2b6d
unknown <namsongc98@gmail.com>
2024-03-21 18:46:39 +0700

    update

--- main/main.js
+++ main/main.js
@@ -10,7 +10,7 @@ import {
   onValue,
 } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBLyi67O-AUbdXZK1wdM0F5Vvi_couK6u0",
   authDomain: "projectdemo-ad6dd.firebaseapp.com",
@@ -23,60 +23,124 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 
+function formatDate(inputDate) {
+  console.log(inputDate);
+  var dateParts = inputDate.split("/");
+  var formattedDate = new Date(dateParts[2], dateParts[0] - 1, dateParts[1]);
+  var day = formattedDate.getDate();
+  var month = formattedDate.getMonth() + 1;
+  var year = formattedDate.getFullYear();
+  var formattedString =
+    (day < 10 ? "0" : "") +
+    day +
+    "/" +
+    (month < 10 ? "0" : "") +
+    month +
+    "/" +
+    year;
+  console.log(formattedString);
+  return formattedString;
+}
+
+const inputDate = new Date();
+const date = inputDate.getDate();
+const month = inputDate.getMonth() + 1;
+const year = inputDate.getFullYear();
+
+const formattedDate = `${year}-${month.toString().padStart(2, "0")}-${date
+  .toString()
+  .padStart(2, "0")}`;
+
 const idUser = document.getElementById("idUser");
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
+const startDate = document.getElementById("startDate");
+const endDate = document.getElementById("endDate");
+const completed = document.getElementById("completed");
+const checkAll = document.getElementById("check-all");
 
-const insbtn = document.getElementById("submit");
-const secbtn = document.getElementById("select");
+startDate.value = formattedDate;
+endDate.value = formattedDate;
+
+function changedateformat(val) {
+  const myArray = val.split("-");
+  let year = myArray[0];
+  let month = myArray[1];
+  let day = myArray[2];
+  let formatteddate = day + "/" + month + "/" + year;
+  return formatteddate;
+}
+
+//const insbtn = document.getElementById("submit");
+const updatebtn = document.getElementById("update");
 const pushbtn = document.getElementById("pushGenerated");
 
-const dbRef = ref(db, "users/");
-function logData() {
-  console.log(123);
-}
-$(document).ready(function() {
-  logData()
-})
-
-function getUser() {
-  let listUser = "";
-  try {
-    onValue(dbRef, (snapshot) => {
-      snapshot.forEach((childSnapshot) => {
-        const childKey = childSnapshot.key;
-        const childData = childSnapshot.val();
-        listUser += `<tr>
-                      <td><input type="checkbox" /></td>
-                      <td>${childKey}</td>
-                      <td>${childData.first_name}</td>
-                      <td>${childData.last_name}</td>
-                      <td>  
-                      <button id="add"  onclick="console.log(1233)">add</button>
-                      <button id="update"  onclick="updateUserData(${childKey})">update</button>
-                      <button id="remove"  onclick="removeUserData(${childKey})">delete</button>
-                      </td>
+document.addEventListener("DOMContentLoaded", () => {
+  const dbRef = ref(db, "users/");
+  async function getUser() {
+    try {
+      await onValue(dbRef, (snapshot) => {
+        let listUser = ``;
+        snapshot.forEach((childSnapshot) => {
+          const childKey = childSnapshot.key;
+          const childData = childSnapshot.val();
+          listUser += `<tr>
+                       <td><input type="checkbox" name="checkbox" id="${childKey}"  onchange="handleCheckbox(this,'${childKey}')" /></td>
+                        <td>${childKey}</td>
+                        <td>${childData.first_name}</td>
+                        <td>${childData.last_name}</td>
+                        <td>${
+                          childData.start_date
+                            ? childData.start_date
+                            : "không có dữ liệu"
+                        }</td>
+                        <td>${
+                          childData.end_date
+                            ? childData.end_date
+                            : "không có dữ liệu"
+                        }</td>
+                        <td> <input type="checkbox"  class="checkbox-input" ${
+                          childData.completed ? "checked" : ""
+                        } id="completed" /></td>
+                        <td>  
+                            <button  onclick="selectData('${childKey}')">select</button>
+                            <button  onclick="removeUserData('${childKey}')">delete</button>
+                        </td>
                     </tr>`;
+        });
+        document.getElementById("tablebody").innerHTML = listUser;
+        const checkboxes = document.getElementsByName("checkbox");
+        checkAll.addEventListener("change", () => {
+          checkboxes.forEach((item) => {
+            item.checked = checkAll.checked;
+          });
+        });
+        window.handleCheckbox = (inputThis) => {
+
+          
+        };
       });
-      document.getElementById("tablebody").innerHTML = listUser;
-    });
-  } catch (error) {
-    console.log(error);
+    } catch (error) {
+      console.log(error);
+    }
   }
-}
-getUser();
+  getUser();
+});
 
-
-const updateUserData = (id) => {
-  console.log("id", id);
-  const firstName = document.getElementById("firstName").value;
-  const lastName = document.getElementById("lastName").value;
-  update(ref(db, "users/" + id), {
-    first_name: firstName,
-    last_name: lastName,
-  }).catch((err) => console.log(err));
+// get user theo id
+window.selectData = (id) => {
+  const dataref = ref(db);
+  get(child(dataref, "users/" + id)).then((snapshot) => {
+    if (snapshot.exists()) {
+      idUser.value = id;
+      firstName.value = snapshot.val().first_name;
+      lastName.value = snapshot.val().last_name;
+    } else {
+      alert("no data");
+    }
+  });
 };
-
+// tạo user
 function writeUserData() {
   set(ref(db, "users/" + idUser.value), {
     id: idUser,
@@ -84,36 +148,32 @@ function writeUserData() {
     last_name: lastName.value,
   }).catch((err) => console.log(err));
 }
-
-function selectData() {
-  const dataref = ref(db);
-  get(child(dataref, "users/" + idUser.value)).then((snapshot) => {
-    if (snapshot.exists()) {
-      firstName.value = snapshot.val().first_name;
-      lastName.value = snapshot.val().last_name;
-    } else {
-      alert("no data");
-    }
-  });
-}
-
+// tạo user auto id
 function pushGenerated() {
   set(push(ref(db, "users/")), {
     first_name: firstName.value,
     last_name: lastName.value,
+    start_date: changedateformat(startDate.value),
+    end_date: changedateformat(endDate.value),
+    completed: completed.checked,
   }).catch((err) => console.log(err));
 }
-
-function removeUserData(id) {
+// update user
+const updateUserData = () => {
+  update(ref(db, "users/" + idUser.value), {
+    first_name: firstName.value,
+    last_name: lastName.value,
+    start_date: changedateformat(startDate.value),
+    end_date: changedateformat(endDate.value),
+    completed: completed.checked,
+  }).catch((err) => console.log(err));
+};
+//xóa user
+window.removeUserData = (id) => {
+  console.log(id);
   remove(ref(db, "users/" + id)).catch((err) => console.log(err));
-}
+};
 
-insbtn.addEventListener("click", writeUserData);
-secbtn.addEventListener("click", selectData);
+//insbtn.addEventListener("click", writeUserData);
+updatebtn.addEventListener("click", updateUserData);
 pushbtn.addEventListener("click", pushGenerated);
-
-
-
-
-
-
